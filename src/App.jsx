3aae6cfb0e90e62b39d2f1e0ecd9d1cbe8c21618
rahyanakil <rahyanakil89@gmail.com>
2025-08-27 21:55:26 +0600

    updated git

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="flex flex-col align-center items-center">
-      <h1 className="pb-6">Counter with Redux</h1>
+      <h1 className="pb-6 text-3xl text-yellow-500">Counter with Redux</h1>
       <button className="bg-cyan-400 p-2 rounded">Increment</button>
       <div className="p-4">0</div>
       <button className="bg-red-400 p-2 rounded">Decrement</button>
